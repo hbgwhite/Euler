9dca7b7e42472798e6e144a5d76616f3c4761290
Gregory White <gwhite@fairwaytech.com>
2017-02-16 13:17:10 -0800

    Euler12 efficiency refactors (removed list, collapsed conditional)

---
+++ Euler/Euler12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Euler
+{
+    public class Euler12 : IEuler
+    {
+        public int Execute()
+        {
+            var triangles = 1;
+            var add = 2;
+            while (!(NumberOfDivisors(triangles) > 500))
+            {
+                triangles = triangles+add++;
+            }
+            return triangles;
+        }
+
+        public int NumberOfDivisors(int value)
+        {
+            var factorCount = 0;
+            var max = (int) Math.Sqrt(value);
+            for (var factor = 1; factor <= max; ++factor)
+            {
+                if (value % factor == 0)
+                {
+                    factorCount = (factor != value / factor)
+                        ? factorCount + 2
+                        : factorCount + 1;
+                }
+            }
+            return factorCount;
+        }
+    }
+}
